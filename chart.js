72290513acaa5974a4a9e17b04ae0be2adb5ba63
ZjW1ll <willianzj21@gmail.com>
2026-05-08 18:12:51 -0300

    Alterações no index para adição dos jogadores favoritos e do gráfico
    melhoria no design pelo css com implementação do vertical para jogadores favoritos
    
    js para fazer o gráfico e aparecer imagem com o tooltip

--- chart.js
+++ chart.js
@@ -13,8 +13,59 @@ Chart.register(
   Legend
 );
 
+// Texto no meio do gráfico
+const centerText = {
+  id: 'centerText',
+
+  beforeDraw(chart) {
+
+    const { ctx } = chart;
+
+    const meta = chart.getDatasetMeta(0);
+
+    if (!meta.data.length) return;
+
+    const x = meta.data[0].x;
+    const y = meta.data[0].y;
+
+    ctx.save();
+
+    // Top 3
+    ctx.font = '700 24px Arial';
+    ctx.fillStyle = '#ffffff';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+
+    ctx.fillText('TOP 3', x, y - 10);
+
+    // Camisas
+    ctx.font = '500 13px Arial';
+    ctx.fillStyle = '#94A3B8';
+
+    ctx.fillText('CAMISAS', x, y + 15);
+
+    ctx.restore();
+  }
+};
+
+// Imagens camisetas
+const jerseyImages = {
+
+  'Curry #30':
+    'https://sheshalifestyle.com/wp-content/uploads/2023/10/boys-icon-swingman-jersey-golden-state-warriors-curry-stephen-nba-ez2b7bx2p00-warsc-ndj.webp',
+
+  'James #23':
+    'https://fanatics.frgimages.com/los-angeles-lakers/unisex-jordan-brand-lebron-james-purple-los-angeles-lakers-2025/26-authentic-jersey-statement-edition_ss5_p-202926266+pv-2+u-lsxjoo0nyrjpe8dzwahj+v-n9ttpegbpfhkd1e5irgt.png?_hv=2&w=1018',
+
+  'Tatum #0':
+    'https://bouncewear.com/cdn/shop/files/AURORA_DN2070-100_PHSFH001-1000.png?v=1691582762'
+};
+
+const tooltipEl = document.getElementById('chart-tooltip');
+
 const ctx = document.getElementById('chart');
 
+// Gráfico
 new Chart(ctx, {
 
   type: 'doughnut',
@@ -30,29 +81,29 @@ new Chart(ctx, {
     datasets: [{
 
       data: [
-        45,
-        30,
-        25
+        12450,
+        10980,
+        8420
       ],
 
       backgroundColor: [
+        '#dfcc27ff',
         '#552583',
-        '#007A33',
-        '#1D428A'
+        '#007A33'
       ],
-      borderColor: [
-        '#7b38d8',
-        '#00b050',
-        '#3b82f6'
+
+      hoverBackgroundColor: [
+
+        '#faf1a4ff',
+        '#7C3AED',
+        '#00A651'
       ],
 
       borderWidth: 0,
 
-      hoverBackgroundColor: [
-        '#7b38d8',
-        '#00b050',
-        '#3b82f6'
-      ]
+      spacing: 4,
+
+      borderRadius: 6
     }]
   },
 
@@ -63,16 +114,65 @@ new Chart(ctx, {
     plugins: {
 
       legend: {
+        display: false
+      },
 
-        labels: {
+      tooltip: {
 
-          color: 'white',
+        enabled: false,
 
-          font: {
-            size: 16
+        external(context) {
+
+          const tooltip = context.tooltip;
+
+          // Esconde tooltip padrão
+          if (tooltip.opacity === 0) {
+
+            tooltipEl.style.opacity = 0;
+            return;
           }
+
+          // Item hover
+          const dataPoint = tooltip.dataPoints[0];
+
+          const label = dataPoint.label;
+
+          const value = dataPoint.raw.toLocaleString();
+
+          const image = jerseyImages[label];
+
+          // Tootlipe criada
+          tooltipEl.innerHTML = `
+            <div class="tooltip-card">
+
+              <div class="tooltip-info">
+
+                <h3>${label}</h3>
+
+                <p>${value} camisetas vendidas</p>
+
+              </div>
+
+              <img src="${image}">
+
+            </div>
+          `;
+
+          // Posição da tooltip criada
+          const { offsetLeft, offsetTop } = ctx;
+
+          tooltipEl.style.opacity = 1;
+
+          tooltipEl.style.left =
+            offsetLeft + tooltip.caretX + 'px';
+
+          tooltipEl.style.top =
+            offsetTop + tooltip.caretY + 'px';
         }
       }
     }
-  }
+  },
+
+  plugins: 
+  [centerText]
 });
